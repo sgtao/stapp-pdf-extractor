b6ac03bc80434b2746a27b7e1dfaee17cca8b3ca
Shogo Ogami <sg.tao.so@gmail.com>
2025-12-07 16:19:00 +0900

    Del:remove example_app page for not used

--- src/main.py
+++ src/main.py
@@ -7,5 +7,6 @@ Edit `/src` and `/tests` to customize this app to your heart's desire :heart:.
 """
 
 # サイドバーのページに移動
-# st.page_link("pages/example_app.py", label="Go to Example App")
-st.page_link("pages/01_example_app.py", label="Go to Example App", icon="🚀")
+st.page_link(
+    "pages/11_pdf_extractor.py", label="Go to PDF Extractor App", icon="📄"
+)
